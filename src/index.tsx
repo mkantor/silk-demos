b1f414e28ce4f4d7544034cbd57f833ccb036331
Matt Kantor <the.matt.kantor@gmail.com>
2025-10-05 14:48:37 -0400

    Turn it into a web server

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,38 @@
-import { createElement } from '@matt.kantor/silk'
+import {
+  type ReadableHTMLTokenStream,
+  createElement,
+  HTMLSerializingTransformStream,
+} from '@matt.kantor/silk'
+import { createServer } from 'node:http'
+import { Writable } from 'node:stream'
 
-const output = (
-  <>
-    {'Hello, '}
-    {'world!'}
-  </>
-)
+const port = 80
 
-for await (const chunk of output) {
-  if (chunk.kind === 'text') {
-    process.stdout.write(chunk.text)
-  }
-}
-process.stdout.write('\n')
+const slowlyGetPlanet = (): Promise<ReadableHTMLTokenStream> =>
+  new Promise((resolve) =>
+    setTimeout(() => resolve(<strong>world</strong>), 2000),
+  )
+
+const server = createServer((_request, response) => {
+  const document = (
+    <html lang="en">
+      <head>
+        <title>Greeting</title>
+      </head>
+      <body>Hello, {slowlyGetPlanet()}!</body>
+    </html>
+  )
+
+  response.setHeader('Content-Type', 'text/html; charset=utf-8')
+  document
+    .pipeThrough(
+      new HTMLSerializingTransformStream({
+        includeDoctype: true,
+      }),
+    )
+    .pipeTo(Writable.toWeb(response))
+    .catch(console.error)
+})
+
+server.listen(port)
+console.log(`Server is listening on port ${port}…`)
