89fe4c9f3ceddc1077a5adfe4992ddb96ea269a0
Matt Kantor <the.matt.kantor@gmail.com>
2025-10-05 17:44:10 -0400

    Implement a filesystem-based router for pages

---
+++ src/content/.page.tsx
@@ -0,0 +1,10 @@
+import { createElement } from '@matt.kantor/silk'
+
+export default () => (
+  <html lang="en">
+    <head>
+      <title>Greeting</title>
+    </head>
+    <body>Hello, world!</body>
+  </html>
+)
--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,67 @@
-import {
-  type ReadableHTMLTokenStream,
-  createElement,
-  HTMLSerializingTransformStream,
-} from '@matt.kantor/silk'
+import { HTMLSerializingTransformStream } from '@matt.kantor/silk'
 import { createServer } from 'node:http'
 import { Writable } from 'node:stream'
 
 const port = 80
 
-const slowlyGetPlanet = (): Promise<ReadableHTMLTokenStream> =>
-  new Promise((resolve) =>
-    setTimeout(() => resolve(<strong>world</strong>), 2000),
+const server = createServer((request, response) => {
+  // The URL constructor normalizes paths such that `url.pathname` will always
+  // be absolute and not contain any `..`s.
+  const url = new URL(
+    `http://${process.env['HOST'] ?? 'localhost'}${request.url}`,
   )
 
-const server = createServer((_request, response) => {
-  const document = (
-    <html lang="en">
-      <head>
-        <title>Greeting</title>
-      </head>
-      <body>Hello, {slowlyGetPlanet()}!</body>
-    </html>
-  )
-
-  response.setHeader('Content-Type', 'text/html; charset=utf-8')
-  document
-    .pipeThrough(
-      new HTMLSerializingTransformStream({
-        includeDoctype: true,
-      }),
-    )
-    .pipeTo(Writable.toWeb(response))
-    .catch(console.error)
+  const pageModulePath = `./content${url.pathname}.page.js`
+  import(pageModulePath)
+    .then((module: unknown) => {
+      if (
+        !(
+          // This validation is unfortunately somewhat limited.
+          (
+            typeof module === 'object' &&
+            module !== null &&
+            'default' in module &&
+            typeof module.default === 'function'
+          )
+        )
+      ) {
+        throw new Error(
+          `${pageModulePath} does not have a default export which is a function`,
+        )
+      } else {
+        const page: unknown = module.default()
+        if (!(page instanceof ReadableStream)) {
+          throw new Error(
+            `Page was ${
+              page === null
+                ? 'null'
+                : page === undefined
+                ? 'undefined'
+                : page.constructor.name !== ''
+                ? page.constructor.name
+                : typeof page
+            }, but expected a ReadableStream`,
+          )
+        } else {
+          // Assume it's a `ReadableStream<HTMLToken>`.
+          response.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return page
+            .pipeThrough(
+              new HTMLSerializingTransformStream({
+                includeDoctype: true,
+              }),
+            )
+            .pipeTo(Writable.toWeb(response))
+            .catch(console.error)
+        }
+      }
+    })
+    .catch((error) => {
+      response.setHeader('Content-Type', 'text/plain')
+      response.write('Page not found')
+      console.error(error)
+      response.end()
+    })
 })
 
 server.listen(port)
