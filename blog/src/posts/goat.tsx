f3ab7306f941ff9f88f342eebb3bee37f23eab0d
Matt Kantor <the.matt.kantor@gmail.com>
2025-10-16 12:47:25 -0400

    More goats

--- blog/src/posts/goat.tsx
+++ blog/src/posts/goat.tsx
@@ -1,13 +1,18 @@
 import { createElement } from '@superhighway/silk'
 import { post } from '../post.js'
 
+const goats = ['/goat1.jpg', '/goat2.jpg', '/goat3.jpg'] as const
+
 export default post(import.meta, {
   title: 'Goat',
   date: new Date('2025-09-30T22:04:24Z'),
-  content: () => (
-    <figure>
-      <img src="/goat.jpg" />
-      <figcaption>Check out this goat.</figcaption>
-    </figure>
-  ),
+  content: () => {
+    const randomGoat = goats[Math.floor(Math.random() * goats.length)]
+    return (
+      <figure>
+        <img src={randomGoat ?? goats[0]} />
+        <figcaption>Check out this goat.</figcaption>
+      </figure>
+    )
+  },
 })
