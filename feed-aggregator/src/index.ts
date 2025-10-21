b07d06ace129994c291ce4302cced0fbab6ecd5a
Matt Kantor <the.matt.kantor@gmail.com>
2025-10-21 12:27:32 -0400

    Support `PORT` environment variable

--- feed-aggregator/src/index.ts
+++ feed-aggregator/src/index.ts
@@ -1,6 +1,6 @@
 import { createServer } from '@superhighway/loom'
 
-const port = 80
+const port = Number(process.env['PORT'] ?? '80')
 
 const server = createServer({
   publicDirectory: `${import.meta.dirname}/public`,
